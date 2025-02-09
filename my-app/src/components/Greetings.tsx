91a98b7efe1a453f93423608b1b5e26fb78e4f50
farroshayray <farros.hr@gmail.com>
2025-02-09 12:28:47 +0700

    update spacing

--- my-app/src/components/Greetings.tsx
+++ my-app/src/components/Greetings.tsx
@@ -4,7 +4,7 @@ const Greetings = () => {
   return (
     <div className='flex justify-center flex-col py-5 h-screen z-20'>
         <img src={require('../pictures/company-logo-bw.png')} alt="Company logo" 
-        className='w-40 h-40 mx-auto mt-16 md:mt-24'/>
+        className='hidden md:block w-40 h-40 mx-auto mt-0 md:mt-24'/>
         <h1>Web Designer and Developer</h1>
         <p className='px-5'>Expertise in React, JavaScript, and modern web technologies</p>
     </div>
