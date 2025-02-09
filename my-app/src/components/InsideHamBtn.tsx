cddb9ceed6b9ecf0e5fb7838667dda45ce3b7d61
farroshayray <farros.hr@gmail.com>
2025-02-09 21:50:51 +0700

    update ham button

--- my-app/src/components/InsideHamBtn.tsx
+++ my-app/src/components/InsideHamBtn.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const InsideHamBtn = () => {
   return (
-    <div className='flex flex-col absolute top-20 right-0'>
-        <button className='p-2 bg-black hover:bg-slate-800'>Inside Ham Button</button>
-        <button className='p-2 bg-black hover:bg-slate-800'>Inside Ham Button</button>
-        <button className='p-2 bg-black hover:bg-slate-800'>Inside Ham Button</button>
-        <button className='p-2 bg-black hover:bg-slate-800'>Inside Ham Button</button>
-        <button className='p-2 bg-black hover:bg-slate-800'>Inside Ham Button</button>
+    <div className='flex flex-col absolute top-20 right-0 bg-opacity-55 bg-black rounded-md'>
+        <button className='p-2 hover:bg-white hover:bg-opacity-40 hover:text-white rounded-md'>Inside Ham Button</button>
+        <button className='p-2 hover:bg-white hover:bg-opacity-40 hover:text-white rounded-md'>Inside Ham Button</button>
+        <button className='p-2 hover:bg-white hover:bg-opacity-40 hover:text-white rounded-md'>Inside Ham Button</button>
+        <button className='p-2 hover:bg-white hover:bg-opacity-40 hover:text-white rounded-md'>Inside Ham Button</button>
+        <button className='p-2 hover:bg-white hover:bg-opacity-40 hover:text-white rounded-md'>Inside Ham Button</button>
     </div>
   )
 }
